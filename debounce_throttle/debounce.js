ab01226952caa975e05396e11acb5fc64dd35ca8
changhong <changhongwang@126.com>
2021-12-19 23:58:55 +0800

    docs: 文档

--- debounce_throttle/debounce.js
+++ debounce_throttle/debounce.js
@@ -6,6 +6,8 @@
  * 2. 用户名、手机号、邮箱输入验证
  * 3. 浏览器窗口大小改变后，只需装口调整完后，再执行resize中的代码，防止重复渲染
  */
+
+// 最简单的防抖函数：设置一个定时器，每当重复调用的时候，就清除定时器，重新定时，直到在设定的时间内没有重复调用函数
 function debounce(fn, gapTime) {
     let timer;
     let baseTime = new Date().getTime();
