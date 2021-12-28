514ec15a9660d27cfa50b2e7ddf6782c3c896f09
changhong <changhongwang@126.com>
2021-12-28 23:53:35 +0800

    手写apply

--- js_programming/handCode_js_base/12_apply.js
+++ js_programming/handCode_js_base/12_apply.js
@@ -1,21 +1,52 @@
-// apply 函数实现
-Function.prototype.myApply = function(context) {
+
+// 手写apply 函数实现 - 核心关键点：转移this指向
+
+/**
+ * 
+ */
+Function.prototype.myApply = function(context, args) {
     // 判断调用对象是否为函数
-    if (typeof this !== "function") {
-      throw new TypeError("Error");
+    if (typeof this !== 'function') {
+        throw new TypeError('调用者不是函数')
     }
     let result = null;
-    // 判断 context 是否存在，如果未传入则为 window
-    context = context || window;
-    // 将函数设为对象的方法
-    context.fn = this;
-    // 调用方法
-    if (arguments[1]) {
-      result = context.fn(...arguments[1]);
-    } else {
-      result = context.fn();
+    if (!context) {
+        context = window;
     }
-    // 将属性删除
+    // 目的 转移调用myApply的函数内的this指向
+    context.fn = this;
+    if (args) {
+        result = context.fn(args);
+    } else {
+        result = context.fn();
+    }
+
+    // 删除属性
     delete context.fn;
     return result;
-  };
+}
+
+function sum(arr) {
+    let total = 0;
+    if (!arr.length) {
+        console.log('数组长度为0 退出');
+        return total;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total;
+}
+
+const resA = sum.myApply(null, [1,2,3]);
+
+const obj = {
+    name: 'wang'
+}
+function sayName() {
+    return this.name;
+}
+const resB = sayName.myApply(obj);
+
+console.log(resA, 'resA');
+console.log(resB, 'resB');
