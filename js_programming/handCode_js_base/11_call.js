759b0e3f88038216d25a3ac9f8731510105ed45a
changhong <changhongwang@126.com>
2021-12-28 23:53:12 +0800

    手写call

--- js_programming/handCode_js_base/11_call.js
+++ js_programming/handCode_js_base/11_call.js
@@ -1,19 +1,17 @@
-// call函数实现
-Function.prototype.myCall = function(context) {
-    // 判断调用对象
-    if (typeof this !== "function") {
-      console.error("type error");
+// 手写call函数实现 - 与apply一样 转移this指向
+Function.prototype.myCall = function(context, ...args) {
+    // 先判断调用对象
+    if (typeof this !== 'function') {
+        console.log('type error');
     }
-    // 获取参数
-    let args = [...arguments].slice(1),
-        result = null;
-    // 判断 context 是否传入，如果未传入则设置为 window
-    context = context || window;
-    // 将调用函数设为对象的方法
+    let res = null;
+    if (!context) {
+        context = window;
+    }
+
     context.fn = this;
-    // 调用函数
-    result = context.fn(...args);
-    // 将属性删除
+    res = context.fn(...args);
+    // 删除属性
     delete context.fn;
-    return result;
-  };
+    return res;
+}
