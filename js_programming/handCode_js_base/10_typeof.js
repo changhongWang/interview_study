87e98f9fe3d59e05694ee92aab64a9de49d339c0
changhong <changhongwang@126.com>
2021-12-29 21:55:17 +0800

    手写typeof

--- js_programming/handCode_js_base/10_typeof.js
+++ js_programming/handCode_js_base/10_typeof.js
@@ -1,16 +1,23 @@
+// 手写typeof
+/**
+ * 核心原理:
+ * 1. typeof 无法判断具体的引用类型，这里利用toString实现 注意要使用原型链上的toString
+ * 2. typeof 无法判断null
+ */
 function getType(value) {
-    // 判断数据是 null 的情况
+    // 判断是null
     if (value === null) {
-      return value + "";
+        // typeof null === 'object'; typeof无法判断null!
+        // 隐式类型转换 输出结果
+        return value + '';
     }
-    // 判断数据是引用类型的情况
-    if (typeof value === "object") {
-      let valueClass = Object.prototype.toString.call(value),
-        type = valueClass.split(" ")[1].split("");
-      type.pop();
-      return type.join("").toLowerCase();
+    // 判断引用类型
+    if (typeof value === 'object') {
+        let valueClass = Object.prototype.toString.call(value);
+        let type = valueClass.split(' ')[1].split('');
+        type.pop();
+        return type.join('').toLowerCase();
     } else {
-      // 判断数据是基本数据类型的情况和函数的情况
-      return typeof value;
+        return typeof value;
     }
-  }
+}
