ac4e61c1481a00e21bf01f46fa4b3ef94d500293
changhong <changhongwang@126.com>
2021-12-29 19:44:21 +0800

    手动实现call方法 - 示例

--- js_programming/handCode_js_base/11_call.js
+++ js_programming/handCode_js_base/11_call.js
@@ -1,4 +1,14 @@
-// 手写call函数实现 - 与apply一样 转移this指向
+// 手写call函数实现 - 核心: 转移this指向
+
+/**
+ * 1. 判断调用对象是否为函数，即使我们是定义在原型上的，还是可能出现call调用等情况
+ * 2. 上下文是否存在，不存在给window
+ * 3. 处理传入的参数，截取第一个参数之后的所有参数
+ * 4. 将函数作为上下文context的一个属性
+ * 5. 使用上下文对象来调用这个方法，并保存返回结果
+ * 6. 删除刚才新增的属性
+ * 7. 返回结果
+ */
 Function.prototype.myCall = function(context, ...args) {
     // 先判断调用对象
     if (typeof this !== 'function') {
@@ -15,3 +25,28 @@ Function.prototype.myCall = function(context, ...args) {
     delete context.fn;
     return res;
 }
+
+function sum(...args) {
+    let total = 0;
+    if (![args].length) {
+        console.log('数组长度为0 退出');
+        return total;
+    }
+    for (let i = 0; i < args.length; i++) {
+        total += args[i];
+    }
+    return total;
+}
+
+const resA = sum.myCall(null, 1,2,3);
+
+const obj = {
+    name: 'wang'
+}
+function sayName() {
+    return this.name;
+}
+const resB = sayName.myCall(obj);
+
+console.log(resA, 'resA');
+console.log(resB, 'resB');
