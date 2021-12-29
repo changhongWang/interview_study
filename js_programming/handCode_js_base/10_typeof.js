b887ccdd56ad49f0b1ddd132307df03c02757ba1
changhong <changhongwang@126.com>
2021-12-29 22:38:03 +0800

    docs: typeof 笔记

--- js_programming/handCode_js_base/10_typeof.js
+++ js_programming/handCode_js_base/10_typeof.js
@@ -13,6 +13,14 @@ function getType(value) {
     }
     // 判断引用类型
     if (typeof value === 'object') {
+        // Javascript中，通过Object.prototype.toString方法，判断某个对象值属于哪种内置类型
+
+        /**
+         * 还可以通过instanceof和constructor进行判断，不过他们都各有一些缺点
+         * instanceof 只能判断引用类型，不可以判断原始类型 / null / undefined
+         * constructor判断类型看起来是完美的，但如果创建一个对象，更改他的原型，这种方式就变的不可靠了。
+         * 最终结论 Object.prototype.toString完美
+         */
         let valueClass = Object.prototype.toString.call(value);
         let type = valueClass.split(' ')[1].split('');
         type.pop();
