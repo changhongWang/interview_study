b3c01737777e96657c80f2657584df08c08d8fb4
changhong <changhongwang@126.com>
2021-12-31 23:22:24 +0800

    柯里化

--- js_programming/handCode_js_base/14_curry.js
+++ js_programming/handCode_js_base/14_curry.js
@@ -1,4 +1,7 @@
 // 函数柯里化指的是一种将使用多个参数的一个函数转换成一系列使用一个参数的函数的技术
-function curry(fn, args) {
+function curry(fn, args = []) {
     const fnArgsLength = fn.length;
+    return function() {
+        
+    }
 }
