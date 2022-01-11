bcb60a97155281ff9181b250c19672bacbf2e2f4
changhong <changhongwang@126.com>
2022-01-11 15:50:46 +0800

    求和方法

--- js_programming/part2_data_handler/4_add.js
+++ js_programming/part2_data_handler/4_add.js
@@ -5,6 +5,8 @@ const arr = [1,2,3,4,5,6,7,8,9,10];
 const sum = arr.reduce((total, i) => total += i, 0);
 console.log(sum)
 
+// arr=[1,2,3,[[4,5],6],7,8,9]，求和
+// 利用flat函数实现
 const isFullyFlatted = (arr) => {
     let isFullyFlatted = true;
     for (let i = 0; i < arr.length; i++) {
@@ -14,9 +16,7 @@ const isFullyFlatted = (arr) => {
     }
     return isFullyFlatted;
 }
-// arr=[1,2,3,[[4,5],6],7,8,9]，求和
 
-// 利用flat函数实现
 const arr2 = [1,2,3,[[4,5],6],7,8,9];
 function flatSum(arr) {
     let flattedArr = arr.flat();
@@ -28,3 +28,38 @@ function flatSum(arr) {
 }
 const sum2 = flatSum(arr2);
 console.log(sum2)
+
+// toString实现
+function arrSumByToString(arr) {
+    const arrString = Array.prototype.toString.call(arr);
+    const newArr = arrString.split(',');
+    const res = newArr.reduce((total, i) => total += Number(i), 0);
+    return res;
+}
+const sum3 = arrSumByToString(arr2);
+console.log(sum3, 'sum3')
+
+// 递归实现
+function recurrenceArrSum(arr, total = 0) {
+    for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] === 'number') {
+            total += arr[i];
+        } else {
+            total = recurrenceArrSum.call(this, arr[i], total);
+        }
+    }
+    return total;
+}
+
+const sum4 = recurrenceArrSum(arr2);
+console.log(sum4, 'sum4')
+
+// 高级递归写法
+const arr5 = [1,2,3,4,5,6];
+function add(arr) {
+    if (arr.length === 1) {
+        return arr[0];
+    }
+    return arr[0] + add(arr.slice(1));
+}
+console.log(add(arr5))
