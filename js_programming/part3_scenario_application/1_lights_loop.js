6466bca97a4b3af114687a313296e39d2ff582c2
changhong <changhongwang@126.com>
2022-01-22 23:55:30 +0800

    循环亮灯题目

---
+++ js_programming/part3_scenario_application/1_lights_loop.js
@@ -0,0 +1,11 @@
+// 循环亮灯
+// 红灯 3s 亮一次，绿灯 1s 亮一次，黄灯 2s 亮一次；如何让三个灯不断交替重复亮灯？
+function red() {
+    console.log('red');
+}
+function green() {
+    console.log('green');
+}
+function yellow() {
+    console.log('yellow');
+}
