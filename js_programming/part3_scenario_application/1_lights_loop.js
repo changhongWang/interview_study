81f9e6eb8d717a97b0d209e3d98855da33765ec1
changhong <changhongwang@126.com>
2022-01-24 16:41:43 +0800

    循环亮灯题目完成

--- js_programming/part3_scenario_application/1_lights_loop.js
+++ js_programming/part3_scenario_application/1_lights_loop.js
@@ -9,3 +9,66 @@ function green() {
 function yellow() {
     console.log('yellow');
 }
+
+function execType(type) {
+    switch(type) {
+        case 'red': red();break;
+        case 'green': green();break;
+        case 'yellow': yellow();break;
+    }
+}
+
+// 递归方式 调用自身 实现
+const task = (timer, light, callback) => {
+    setTimeout(() => {
+        switch(light) {
+            case 'red': red();break;
+            case 'green': green();break;
+            case 'yellow': yellow();break;
+        };
+        callback();
+    }, timer);
+}
+
+const step = () => {
+    task(3000, 'red', () => {
+        task(2000, 'green', () => {
+            task(1000, 'yellow', step)
+        })
+    })
+};
+// step();
+
+// 递归+promise方式实现
+const task1 = (time, light) => new Promise((resolve, reject) => {
+    setTimeout(() => {
+        switch(light) {
+            case 'red': red();break;
+            case 'green': green();break;
+            case 'yellow': yellow();break;
+        };
+        resolve();
+    }, time)
+})
+
+const step1 = () => {
+    task1(3000, 'red').then(() => {
+        task1(2000, 'green').then(() => {
+            task1(1000, 'yellow').then(() => {
+                step1();
+            })
+        })
+    })
+}
+
+// step1();
+
+// async await实现
+async function loop3() {
+    await task1(3000, 'red');
+    await task1(2000, 'green');
+    await task1(1000, 'yellow');
+    await loop3();
+}
+
+loop3();
